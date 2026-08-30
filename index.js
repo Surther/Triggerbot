aaaa8f8b19f0d4be741abac91f6d8b694259b385
Surther <pandey.sumit2357@gmail.com>
2026-08-30 14:13:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits, ChannelType } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+const CHANNEL_ID = "1508029068363300885";
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+    if (message.channel.id !== CHANNEL_ID) return;
+
+    const isThread =
+        message.channel.type === ChannelType.PublicThread ||
+        message.channel.type === ChannelType.PrivateThread;
+
+    if (!isThread) {
+        const warning = await message.channel.send(
+            `hey ${message.author} 👋 please keep discussion inside threads!`
+        );
+        setTimeout(() => warning.delete(), 8000);
+    }
+});
+
+client.login(process.env.TOKEN);
