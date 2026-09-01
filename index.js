4e3841643c7e06ce00cfebd65caab1d747fd6596
Surther <pandey.sumit2357@gmail.com>
2026-09-01 20:59:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
 
 const CHANNEL_ID = "1508029068363300885";
 const ROLE_CHANNEL_ID = "1118955072756392008";
-const REACTION_MESSAGE_ID = "1544366236173869057";
+const REACTION_MESSAGE_ID = "1544368281694113864";
 const messageCount = {};
 
 const reactionRoles = [
@@ -22,7 +22,7 @@ const reactionRoles = [
     { emoji: "scam_origins:1509624479708352653", roleId: "1153758935300440064" },
     { emoji: "pepembmbusiness:1533158525650604226", roleId: "1533140085539864626" },
     { emoji: "pepegamer:1533143536554348796", roleId: "1533140088609837217" },
-    { emoji: "coin_origin:1538876611250626673", roleId: "1533175818832973825" },
+    { emoji: "coin_origin:1538876611250626673", roleId: "1538876893023969391" },
 ];
 
 const roles = [
